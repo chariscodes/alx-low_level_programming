43f0e18408d1b31527b7b52eeebeda5e590de586
chariscodes <charisoneyemiot@gmail.com>
2023-04-25 13:40:06 -0700

    Write a function that prints the n times table, starting with 0. Prototype: void print_times_table(int n); If n is greater than 15 or less than 0 the function should not print anything

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,51 @@
-include "main.h"
+#include "main.h"
 
 /**
- * print_times_table - Prints the n times table
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The highest multiple to be printed.
  *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+ * Return: void.
  */
 void print_times_table(int n)
 {
-	int a, b, op;
+	int i, j;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (a = 0; a <= n; a++)
+		for (j = 0; j <= n; j++)
 		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
+			if (j == 0)
+				_putchar('0');
+			else
 			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
+				int res = i * j;
+				_putchar(',');
+				_putchar(' ');
+
+				if (res < 10)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
 				}
-				else if (op <= 99)
+				else if (res < 100)
 				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
 				}
 				else
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(res / 100 + '0');
+					_putchar((res % 100) / 10 + '0');
+					_putchar(res % 10 + '0');
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
+
