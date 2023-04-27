4e1c6ecc2a582632b5d9f146b04f30571317adc9
chariscodes <charisoneyemiot@gmail.com>
2023-04-27 16:17:39 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,31 +6,30 @@
  */
 int main(void)
 {
-	int n;
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	int x;
+
+	for (x = 1; x <= 100; x++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (n % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (n % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", n);
-		}
-		if (x != 100)
-		{
-			printf(" ");
-		}
+	if ((x % 3 == 0) && (x % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (x % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (x % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", x);
+	}
+	if (x != 100)
+	{
+	print(" ");
+	}
 	}
 	printf("\n");
 	return (0);
