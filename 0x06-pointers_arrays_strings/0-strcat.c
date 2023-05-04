97e2d289e2bd5458cde6fdfabeecc63cb1d06482
chariscodes <charisoneyemiot@gmail.com>
2023-05-04 16:44:06 -0700

    Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,38 @@
 #include "main.h"
-
 /**
- * _strncat - Concatenates two strings using at most
- * an inputted number of bytes from src
- * @dest: The string to be appended upon
- * @src: The string to be appended to dest
- * @n: The number of bytes from src to be appended to dest
- * Return: A pointer to the resulting string dest
+ * _strcat - Concatenates two strings.
+ *
+ * @dest: A pointer to the destination string.
+ * @src: A pointer to the source string.
+ *
+ * Return: A pointer to the resulting string (dest).
+ *
+ * Description: This function appends the src string to the dest string,
+ * overwriting the terminating null byte ('\0') at the end of dest, and then
+ * adds a terminating null byte. It returns a pointer to the resulting string.
  */
-char *_strncat(char *dest, char *src, int n)
-{
-	int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	/* Find the end of the destination string */
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Append the source string to the destination string */
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add the terminating null byte */
+	dest[i] = '\0';
+	/* Return a pointer to the resulting string */
 	return (dest);
 }
