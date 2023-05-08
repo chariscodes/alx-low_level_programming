eb02fe4e294efc016c978db6431a16fd54ce1463
chariscodes <charisoneyemiot@gmail.com>
2023-05-08 13:01:19 -0700

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,30 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * Return: Nothing.
+ *Return: copied memory with n byted changed
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
+        int r = 0;
+        int i = n;
+        for (; r < i; r++)
         {
-            printf(" ");
+                dest[r] = src[r];
+                n--;
         }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+        return (dest);
 }
