207c7094687ccaa23407318455dca36e171e54f8
chariscodes <charisoneyemiot@gmail.com>
2023-05-18 11:44:32 -0700

    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,49 +1,99 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-
-#define ERR_MSG "Error"
-
-int _isdigit(char *s);
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Takes two numbers as arguments and outputs the product.
- * @argc: The number of arguments including the name of the program.
- * @argv: An array of arguments.
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: 0 if successful.
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int main(int argc, char *argv[])
+int is_digit(char *s)
 {
-	int i;
+	int i = 0;
 
-	if (argc != 3)
+	while (s[i])
 	{
-		for (i = 0; ERR_MSG[i] != '\0'; i++)
-			_putchar(ERR_MSG[i]);
-		_putchar('\n');
-		exit(98);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
-
-	if (_isdigit(argv[1]) == 0)
-		printf("%s\n", argv[1]);
-
-	return (0);
+	return (1);
 }
 
 /**
- * _isdigit - Checks if a string is a number or not.
- * @s: The string to be checked.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: 1 if true, else 0.
+ * Return: the length of the string
  */
-int _isdigit(char *s)
+int _strlen(char *s)
 {
-	while (*s)
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s < '0' || *s > '9')
-			return (1);
-		s++;
+		i++;
 	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
