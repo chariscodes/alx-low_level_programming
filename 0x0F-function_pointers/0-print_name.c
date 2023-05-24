e1bf60661a14c7187a2a96c4192d38f8ce6a7f3f
chariscodes <charisoneyemiot@gmail.com>
2023-05-24 13:45:53 -0700

    Write a function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,35 @@
-#include "function_pointers.h"
 #include <stdio.h>
 
-/**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
- **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
+    if (name != NULL && f != NULL)
+        f(name);
+}
 
-	f(name);
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+void print_name_uppercase(char *name)
+{
+    unsigned int i = 0;
+    printf("Hello, my uppercase name is ");
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+            putchar(name[i] + 'A' - 'a');
+        else
+            putchar(name[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Alice", print_name_uppercase);
+
+    return 0;
 }
